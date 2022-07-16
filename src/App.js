278b4118aea0075612f5779f9f0fba856cee2d5e
ryanmpdev <ryanmaciel697@gmail.com>
2022-07-16 09:53:04 -0300

    map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+
+  state = {
+    jogadores: [
+      {
+        nome: "Neymar",
+        idade: 30,
+        time: "PSG"
+      },
+      {
+        nome: "Messi",
+        idade: 35,
+        time: "PSG"
+      },
+      {
+        nome: "Cristiano Ronaldo",
+        idade: 37,
+        time:  "Manchester United"
+      },
+      {
+        nome: "Vinícius Júnior",
+        idade: 22,
+        time:"Real Madrid"
+      },
+      {
+        nome: "Giorgian De Arrascaeta",
+        idade: 28,
+        time: " Flamengo"
+      },
+      {
+        nome: "João Gomes",
+        idade: 21,
+        time:"Flamengo"
+      }
+    ]
+  }
+  
+  render(){
+    return(
+      <nav>
+        {this.state.jogadores.map((item) => (
+          <section className="card">
+            <ul>
+              <ol>{item.nome}, {item.idade}, {item.time}</ol>
+            </ul>
+          </section>
+        ))}
+      </nav>
+    )
+  }
 }
-
-export default App;
